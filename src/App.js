825b5cf18ef01f2bba6afd4f58938ce958e0fad4
IliaMelinchyk <sm1tphoen1x@gmail.com>
2023-02-02 20:29:14 +0300

    first ver

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { staticFunnyWheel } from './staticFunnyWheel';
+import React from 'react';
+import { currentFunnyWheel } from './currentFunnyWheel';
+
+const numberOfCycles = 5
+const itemHeight = 100
 
 function App() {
+  const [ winnerData, setWinnerData ] = React.useState(null);
+  const [ winnerPosition, setWinnerPosition ] = React.useState(0);
+  const [ startedRoll, setStartedRoll ] = React.useState(false);
+  React.useEffect(() => {
+    setWinnerData(staticFunnyWheel.find(item => item._id === currentFunnyWheel.winner.id));
+  },[ winnerData ]);
+  
+  const itemsMarkup = React.useMemo(() =>
+	{
+    if (!winnerData)
+      return;
+
+    console.log(winnerData)
+    const itemsForAWheel = [];
+    currentFunnyWheel.list.forEach(id => {
+      const curItemData = staticFunnyWheel.find(item => item._id === id);
+      if (!curItemData)
+        return;
+      itemsForAWheel.push(curItemData?._id);
+    });
+
+    const markup = [];
+    for (let i = 1; i <= numberOfCycles; i++) {
+      const randomizedItemsForAWheel = itemsForAWheel.map(value => ({ value, sort: Math.random() })).sort((a, b) => a.sort - b.sort).map(({ value }) => value);
+  
+      for (let j = 0; j <= randomizedItemsForAWheel.length - 1; j++)
+        markup.push(<div key={"slot_item-" + i + "-" + j} style={{width: "300px", height: `${itemHeight}px`, background: "gray"}}>{randomizedItemsForAWheel[j]}</div>);
+    }
+
+    setWinnerPosition(markup.length * itemHeight)
+
+
+		return markup;
+	}, [winnerData]);
+  
+  const roll = React.useMemo(() =>
+	{
+    setStartedRoll(true)
+	}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button style={{position: "absolute", left: "0px", right: "0px"}} onClick={roll}>ROLL</button>
+      <div className="container">
+        <div className="window" style={{height: `${itemHeight}px`}}>
+          <div className={`items`} style={{bottom: `${startedRoll ? winnerPosition : 0}px`}}>
+          {itemsMarkup}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
