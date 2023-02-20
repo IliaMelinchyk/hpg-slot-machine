0627868fe68af1b0b02ea0310d8974ebdd23621d
IliaMelinchyk <sm1tphoen1x@gmail.com>
2023-02-21 00:21:02 +0300

    old consept unfinished

--- src/App.js
+++ src/App.js
@@ -13,16 +13,19 @@ const widthModificator = 30;
 // Модификатор скорости - чем он выше тем быстрее крутится колесо
 const speedModificator = 10;
 // Количество секунд, на протяжении которых будет крутиться колесо
-const rollLength = 30;
+const rollLength = 1;
 // Количество колес
 const howManyWheels = 3;
 // Расстояние между колесами в пикселях
 const gapBetweenWheelsWidth = 5;
+// Количество секунд между анимациями кручения каждого отдельного колеса
+const wheelAnimationDelay = 0.3;
 
 
 function App() {
   const [ winnerData, setWinnerData ] = React.useState(null);
   const [ startedRoll, setStartedRoll ] = React.useState(false);
+  const [ winnerAnimation, setWinnerAnimation ] = React.useState(false);
   const [ itemHeight, setItemHeight ] = React.useState(0);
   const arraysOfItemsRef = React.useRef([]);
   const [ volchokSound, volchokSoundData] = useSound(volchok, { volume: 0.2 });
@@ -34,6 +37,9 @@ function App() {
     volchokSound();
     setStartedRoll(true);
     setTimeout(() => volchokSoundData.stop(), rollLength * 1000);
+    setTimeout(() => {
+
+    }, (rollLength + wheelAnimationDelay * howManyWheels) * 1000);
 	}, [startedRoll, volchokSound, volchokSoundData]);
 
 
@@ -65,15 +71,64 @@ function App() {
     while (itemsForAWheel.length < 50)
       itemsForAWheel.push(...usedItemsWithoutWinner);
 
+    // Определение выигрышного паттерна, в котором будут расположены победные ячейки
+    // Всего 5 вариантов паттерна
+    // 1 - Победные ячейки в центре
+    // 2 - Победные ячейки в снизу
+    // 3 - Победные ячейки сверху
+    // 4 - Победные ячейки наискосок с нижнего левого угла в правый верхний
+    // 5 - Победные ячейки наискосок с левого верхнего угла в правый нижний
+    const randomWinnerPattern = 1;
+    // const randomWinnerPattern = Math.floor(Math.random() * 5) + 1;
+
     // Перемешиванние массива предметов (при этом два предмета подряд не попадаются), добавление в конец массива победителя и повторение этого действия для каждого из колес
     if (arraysOfItemsRef.current.length === 0) {
       for (let i = 0; i < howManyWheels; i++) {
         arraysOfItemsRef.current.push(shuffleArrayWithoutDuplicates(itemsForAWheel));
-        arraysOfItemsRef.current[i].push(tempWinnerData?.image);
+        switch (randomWinnerPattern) {
+          case 1:
+          default:
+            arraysOfItemsRef.current[i].unshift(tempWinnerData?.image);
+            break;
+          case 2:
+            arraysOfItemsRef.current[i].push(tempWinnerData?.image);
+            break;
+          case 3:
+            arraysOfItemsRef.current[i].splice(1, 0, tempWinnerData?.image);
+            break;
+          case 4:
+            switch (i) {
+              case 0:
+              default:
+                arraysOfItemsRef.current[i].push(tempWinnerData?.image);
+                break;
+              case 1:
+                arraysOfItemsRef.current[i].unshift(tempWinnerData?.image);
+                break;
+              case 2:
+                arraysOfItemsRef.current[i].splice(1, 0, tempWinnerData?.image);
+                break;
+            }
+            break;
+          case 5:
+            switch (i) {
+              case 0:
+              default:
+                arraysOfItemsRef.current[i].splice(1, 0, tempWinnerData?.image);
+                break;
+              case 1:
+                arraysOfItemsRef.current[i].unshift(tempWinnerData?.image);
+                break;
+              case 2:
+                arraysOfItemsRef.current[i].push(tempWinnerData?.image);
+                break;
+            }
+            break;
+        }
       }
     }
 
-    const howManyItems = arraysOfItemsRef.current[0].length - 1;
+    const howManyItems = arraysOfItemsRef.current[0].length;
     const diameter = howManyItems * widthModificator;
     const radius = diameter / 2;
     const angle = 360 / howManyItems;
@@ -81,26 +136,26 @@ function App() {
     const height = circumference / howManyItems;
     const iterationCount = Math.ceil(rollLength / howManyItems * speedModificator);
     const singleTurnDur = rollLength / iterationCount;
-    const wheelStyle = { height: `${height * 4}px`, width: `${height}px`, transformOrigin: `50% calc(50% + ${height / 2}px)`, marginTop: `-${height}px`, animationDuration: `${singleTurnDur}s`, animationIterationCount: iterationCount};
+    const wheelStyle = { height: `${height * 4}px`, width: `${height}px`, transformOrigin: `50% calc(50% + ${height / 2}px)`, marginTop: `-${height}px`, animationDuration: `${singleTurnDur}s`, animationIterationCount: iterationCount };
     const wheelItemStyle = { height: `${height}px`, width: `${height}px` };
     setItemHeight(height);
   
     const markup = [];
     for (let i = 0; i < howManyWheels; i++) {
-      markup.push(<Wheel key={i} delay={i * 0.3} shuffledWheel={arraysOfItemsRef.current[i]} startedRoll={startedRoll} radius={radius} angle={angle} wheelStyle={wheelStyle} wheelItemStyle={wheelItemStyle} />);
+      markup.push(<Wheel key={i} delay={i * wheelAnimationDelay} shuffledWheel={arraysOfItemsRef.current[i]} startedRoll={startedRoll} radius={radius} angle={angle} wheelStyle={wheelStyle} wheelItemStyle={wheelItemStyle} isLastWheel={i === howManyWheels - 1} winnerAnimation={winnerAnimation} setWinnerAnimation={setWinnerAnimation} />);
     }
     return markup;
-  },[startedRoll])
+  },[startedRoll, winnerAnimation])
 
   return (
     <div className="App">
       <div className="container">
-        <div className="wheel" style={{width: `${itemHeight * 3  + gapBetweenWheelsWidth * (howManyWheels - 1)}px`, gap: `${gapBetweenWheelsWidth}px`}}>
+        <div className="wheel" style={{width: `${itemHeight * howManyWheels  + gapBetweenWheelsWidth * (howManyWheels - 1)}px`, gap: `${gapBetweenWheelsWidth}px`}}>
           {wheelsMarkup}
-          <div style={{position: "absolute", height: `${itemHeight}px`, width: `${itemHeight * 3  + gapBetweenWheelsWidth * (howManyWheels - 1)}px`, borderTop: "1px solid red", borderBottom: "1px solid red"}}></div>
+          <div style={{position: "absolute", height: `${itemHeight}px`, width: `${itemHeight * howManyWheels  + gapBetweenWheelsWidth * (howManyWheels - 1)}px`, borderTop: "1px solid red", borderBottom: "1px solid red"}}></div>
         </div>
       </div>
-      <img src={slot} alt="" className='wheel__bg'/>
+      {/* <img src={slot} alt="" className='wheel__bg'/> */}
       <button style={{position: "absolute", left: "0px", right: "0px", margin: "300px auto 0"}} onClick={rollWheel}>ROLL</button>
     </div>
   );
--- src/Wheel.js
+++ src/Wheel.js
@@ -3,23 +3,30 @@ import React from 'react';
 import useSound from 'use-sound'
 import coin from './coin.m4a'
 
-const Wheel = ({ startedRoll, shuffledWheel, delay, radius, angle, wheelStyle, wheelItemStyle }) => {
+const Wheel = ({ startedRoll, shuffledWheel, delay, radius, angle, wheelStyle, wheelItemStyle, isLastWheel, winnerAnimation, setWinnerAnimation }) => {
     const [ coinSound ] = useSound(coin, { volume: 0.2 });
 
     const itemsMarkup = React.useMemo(() => {
         const markup = [];
 
         for (let i = 0; i < shuffledWheel.length; i++) {
-            markup.push(<div key={"slot_item-" + i} style={{ ...wheelItemStyle, transform: `rotateX(${angle * i}deg) translateZ(${radius}px)` }} className="wheel__segment">
+            markup.push(<div key={"slot_item-" + i} style={{ ...wheelItemStyle, transform: `rotateX(${angle * i}deg) translateZ(${radius}px)` }} className={`wheel__segment ${winnerAnimation && i === 0 ? "wheel__segment-winner" : ""}`}>
                 <img style={{ ...wheelItemStyle, objectFit: "contain"}} src={process.env.PUBLIC_URL  + "./wheelImgs/" + shuffledWheel[i] } alt=""/>
             </div>);
         }
 
         return markup;
-	}, [angle, radius, shuffledWheel, wheelItemStyle]);
+	}, [angle, radius, shuffledWheel, wheelItemStyle, winnerAnimation]);
+
+    const onRollEnd = React.useCallback(() =>
+	{
+        coinSound();
+        if (isLastWheel)
+            setWinnerAnimation(true);
+	}, [coinSound, isLastWheel, setWinnerAnimation]);
 
     return <div className={startedRoll ? "wheel__container-appear" : "wheel__container"} style={{animationDelay: `${delay}s`}}>
-        <div style={{...wheelStyle, animationDelay: `${delay}s`}} className={`wheel__inner ${startedRoll ? "wheel__inner-spin" : ""}`} onAnimationEnd={coinSound}>
+        <div style={{...wheelStyle, animationDelay: `${delay}s`}} className={`wheel__inner ${startedRoll ? "wheel__inner-spin" : ""}`} onAnimationEnd={onRollEnd}>
             {itemsMarkup}
         </div>
     </div>
